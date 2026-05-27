95d3fb897be5778cf31e552285806a98884e9c90
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-05-27 13:02:57 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from app.routers import upload
 
 app = FastAPI(
     title="Analisis IO LPM",
     version="0.1.0"
 )
+
+app.include_router(upload.router)
 
 @app.get("/")
 def root():
